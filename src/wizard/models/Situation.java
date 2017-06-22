2d86d51c03e925cd329f78b7b43e19dde6307fac
ifartglitter <j.sziegaud@gmail.com>
2017-06-22 12:52:12 +0200

    [TASK] Situation extended

--- src/wizard/models/Situation.java
+++ src/wizard/models/Situation.java
@@ -9,6 +9,14 @@ public class Situation {
     private int duration;
     private int start;
     private int end;
+    private String scenario; // Auftrag vom Firmenchef an die Fachabteilung ... wall of text
+    private String outcome; // Handlungsprodukt/Lernergebnis:
+    private String competences; // Die Schüler planen die Aufgaben und Reihenfolge eines Netzwerkes. ... wall of text
+    private String content; // Erstellen einer Aufgabenliste zur Einrichtung des ... wall of text
+    private String materials; // Unterrichtsmaterialien;
+    private String comments; // OrganisatorischeHinweise;
+    private String techniques; // LernUndArbeitstechniken;
+    private String achievment; // Leistungsnachweis;
 
     public Situation(String id, String name, int duration, int start, int end) {
         this.id = id;
@@ -56,5 +64,70 @@ public class Situation {
 
     public void setEnd(int end) {
         this.end = end;
+    }
+
+
+    public String getScenario() {
+        return scenario;
+    }
+
+    public void setScenario(String scenario) {
+        this.scenario = scenario;
+    }
+
+    public String getOutcome() {
+        return outcome;
+    }
+
+    public void setOutcome(String outcome) {
+        this.outcome = outcome;
+    }
+
+    public String getCompetences() {
+        return competences;
+    }
+
+    public void setCompetences(String competences) {
+        this.competences = competences;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(String materials) {
+        this.materials = materials;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getTechniques() {
+        return techniques;
+    }
+
+    public void setTechniques(String techniques) {
+        this.techniques = techniques;
+    }
+
+    public String getAchievment() {
+        return achievment;
+    }
+
+    public void setAchievment(String achievment) {
+        this.achievment = achievment;
     }
 }
