85d3bb658531e1c47be76b2f5ab3502408521a27
obi-jan-kenobi <j.sziegaud@gmail.com>
2017-06-26 08:53:34 +0200

    Field & Situtaion -repo added

--- src/wizard/repositories/FieldSQLRepository.java
+++ src/wizard/repositories/FieldSQLRepository.java
@@ -1,7 +1,27 @@
 package wizard.repositories;
 
+import org.sql2o.Connection;
+import wizard.models.Field;
+import wizard.models.Subject;
+import wizard.services.DBServiceProvider;
+
+import java.util.List;
+
 /**
  * Created by jansziegaud on 26.06.17.
  */
 public class FieldSQLRepository {
+    private DBServiceProvider dbServiceProvider;
+
+    public FieldSQLRepository(DBServiceProvider dbServiceProvider) {
+        this.dbServiceProvider = dbServiceProvider;
+    }
+
+    public List<Field> getAllFieldsForSubject(Subject s) {
+        try (Connection con = (Connection)dbServiceProvider.open()) {
+            con.createQuery(String.format("" +
+                    "SELECT * FROM %1s" +
+                    "WHERE")).executeAndFetch(Field.class);
+        }
+    }
 }
--- src/wizard/repositories/SituationSQLRepository.java
+++ src/wizard/repositories/SituationSQLRepository.java
@@ -1,7 +1,32 @@
 package wizard.repositories;
 
+import org.sql2o.Connection;
+import wizard.models.Field;
+import wizard.models.Situation;
+import wizard.services.DBServiceProvider;
+
+import java.util.List;
+
 /**
  * Created by jansziegaud on 23.06.17.
  */
 public class SituationSQLRepository {
+    private DBServiceProvider dbServiceProvider;
+
+    public SituationSQLRepository(DBServiceProvider dbServiceProvider) {
+        this.dbServiceProvider = dbServiceProvider;
+    }
+
+    List<Situation> getAllSitutationsForField(Field f) {
+        try (Connection con = (Connection)dbServiceProvider.open()) {
+            return con.createQuery(String.format(
+                                "SELECT * FROM %1s " +
+                                "JOIN %2s ON %1s.LF_NR = %2s.%3s " +
+                                "WHERE LFID = :field_id;",
+                    Constants.SITUATION_TABLENAME,
+                    Constants.FIELD_TABLENAME,
+                    Constants.FIELD_ID,
+                    f.getId())).executeAndFetch(Situation.class);
+        }
+    }
 }
