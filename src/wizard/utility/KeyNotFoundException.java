37fe2a9de11cc2a0047aa79c78406fdb799ea16c
Fabian Engels <fechiari@byom.de>
2017-06-27 08:56:33 +0200

    [Task] Add KeyNotFoundException to better notify callers of missing keys

---
+++ src/wizard/utility/KeyNotFoundException.java
@@ -0,0 +1,15 @@
+package wizard.utility;
+
+
+/**
+ *  @author F. Engels
+ *  TODO@all: description
+ */
+public class KeyNotFoundException extends Exception {
+
+    public KeyNotFoundException(String message)
+    {
+        super(message);
+    }
+
+}
