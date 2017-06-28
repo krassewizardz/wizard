00a21dd47834f329abb174cac5f0bf1d4102deff
obi-jan-kenobi <j.sziegaud@gmail.com>
2017-06-28 10:13:33 +0200

    idk

--- src/wizard/services/SQL2ODBServiceProvider.java
+++ src/wizard/services/SQL2ODBServiceProvider.java
@@ -9,9 +9,19 @@ import org.sql2o.Sql2o;
 public class SQL2ODBServiceProvider implements DBServiceProvider<Connection> {
 
     private static Sql2o sql2o;
+    private static ConfigServiceProvider configServiceProvider = new JSONConfigService("config.json");
 
     static {
-        sql2o = new Sql2o("mysql://localhost:3306/didakt", "root", "");
+        try {
+            sql2o = new Sql2o(
+                    configServiceProvider.get("db.wizard.host"),
+                    configServiceProvider.get("db.wizard.user"),
+                    configServiceProvider.get("db.wizard.password")
+            );
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Override
