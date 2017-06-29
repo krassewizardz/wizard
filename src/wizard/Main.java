eb8da84c5fac9c864d42332621fbd72b0dbf9f24
obi-jan-kenobi <j.sziegaud@gmail.com>
2017-06-29 12:48:20 +0200

    jar laeuft

--- src/wizard/Main.java
+++ src/wizard/Main.java
@@ -9,6 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Class.forName("org.sqlite.JDBC");
         ViewManager viewManager = ViewManager.getInstance();
         viewManager.setPrimaryStage(stage);
         viewManager.display(View.LOGIN);
